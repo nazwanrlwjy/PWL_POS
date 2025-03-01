9be17b13dc28e3481eeba4d330137fb9ccc0bff6
Nazwa Nurul Wijaya <nazwanrlwjy@gmail.com>
2025-03-02 00:30:26 +0700

    PRAK4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
+
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/level', [LevelController::class, 'index']);
 
 /*
 |--------------------------------------------------------------------------
