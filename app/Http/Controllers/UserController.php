dd70a992a03946b4f44c01240e7008d4b16fcbbd
Nazwa Nurul Wijaya <nazwanrlwjy@gmail.com>
2025-03-09 15:19:56 +0700

    PRAK4_2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,18 +10,57 @@ class UserController extends Controller
 {
     public function index()
     {
+        //JS4_2.3
+        $user = UserModel::where('level_id', 2)->count(); // Menghitung jumlah pengguna
+        return view('user', ['data' => $user]); // Mengirim data ke view
+        
+        // $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
+        // return view('user', ['data' => $user]);
+
+        //JS4_2.2
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOrFail(1);
+        // return view('user', ['data' => $user]);
+
+        //JS4_PRAK2_10
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+
+        // return view('user', ['data' => $user]);
+        //JS4_PRAK2_8
+        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+
+        // return view('user', ['data' => $user]);
+
+        //JS4_PRAK 2.1_6
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        //JS4_PRAK 2.1_4
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
         // JS4_PRAK1_2
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
 
-        UserModel::create($data);
+        // UserModel::create($data);
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::all();
+
+        //Prak 4_2.1
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
 
 
         // // ini JS 3
@@ -33,4 +72,3 @@ class UserController extends Controller
         // return view('user', ['data' => $user]);
     }
 }
-
