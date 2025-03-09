0ade33720d8e0663a917b7d26c9e58ca57ef9f39
Nazwa Nurul Wijaya <nazwanrlwjy@gmail.com>
2025-03-09 19:33:17 +0700

    JS4_PRAK2.6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,49 @@ class UserController extends Controller
 {
     public function index()
     {
-
-        //JS4_2.4
-        $user = UserModel::firstOrNew([
-            'username' => 'manager33',
-            'nama' => 'Manager Tiga Tiga',
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
-            'level_id' => 2
+            'level_id' => 2,
         ]);
+        
+        $user->username = 'manager12';
+        
         $user->save();
-        return view('user', ['data' => $user]);
+        
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username'])); // true
+        
+        //JS4_2.5
+        // $user = UserModel::create([
+        //     'username' => 'manager55',
+        //     'nama' => 'Manager55',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+        
+        // $user->username = 'manager56';
+        
+        // $user->isDirty(); // true
+        // $user->isDirty('username'); // true
+        // $user->isDirty('nama'); // false
+        // $user->isDirty(['nama', 'username']); // true
+        
+        // $user->isClean(); // false
+        // $user->isClean('username'); // false
+        // $user->isClean('nama'); // true
+        // $user->isClean(['nama', 'username']); // false
+        
+        // $user->save();
+        
+        // $user->isDirty(); // false
+        // $user->isClean(); // true
+        // dd($user->isDirty());
+        
         
         // $user = UserModel::firstOrCreate(
         //     [
