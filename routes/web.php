528e686492ecaeb245008833520c320cf24617ef
nazwanrlwjy <nazwanrlwjy@gmail.com>
2025-04-28 23:02:33 +0700

    REVISI JS 5_Prak 2

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,12 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [WelcomeController::class,'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
