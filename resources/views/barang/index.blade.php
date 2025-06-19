c407b3ea0c7fd6bb329ca0ebc9157f01ea807f54
nazwanrlwjy <nazwanrlwjy@gmail.com>
2025-06-20 02:39:06 +0700

    Tugas JS 6_m_barang

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -1,73 +1,32 @@
 @extends('layouts.template')
 
 @section('content')
-    <style>
-        th, td {
-            vertical-align: middle !important;
-            white-space: nowrap;
-        }
-
-        th:first-child,
-        td:first-child {
-            width: 40px;
-            text-align: center;
-        }
-
-        th:nth-child(2),
-        td:nth-child(2) {
-            width: 120px;
-        }
-
-        th:nth-child(3),
-        td:nth-child(3) {
-            width: 150px;
-        }
-
-        th:nth-child(4),
-        td:nth-child(4) {
-            width: 100px;
-        }
-
-        th:nth-child(5),
-        td:nth-child(5),
-        th:nth-child(6),
-        td:nth-child(6) {
-            text-align: right;
-            width: 100px;
-        }
-
-        th:last-child,
-        td:last-child {
-            text-align: center;
-            width: 90px;
-        }
-    </style>
-
     <div class="card card-outline card-primary">
         <div class="card-header">
-            <h3 class="card-title">{{ $page->title }}</h3>
+            <h3 class="card-title">Daftar Barang</h3>
             <div class="card-tools">
                 <a class="btn btn-sm btn-primary mt-1" href="{{ url('barang/create') }}">Tambah</a>
+                <button onclick="modalAction('{{url('barang/create_ajax')}}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
             </div>
         </div>
         <div class="card-body">
             @if (session('success'))
                 <div class="alert alert-success">{{ session('success') }}</div>
             @endif
-
+            
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
-
+            
             <div class="row">
                 <div class="col-md-12">
                     <div class="form-group row">
                         <label class="col-1 control-label col-form-label">Filter:</label>
                         <div class="col-3">
-                            <select class="form-control" id="kategori_id" name="kategori_id">
-                                <option value="">- Semua -</option>
+                            <select class="form-control" id="kategori_id" name="kategori_id" required>
+                                <option value="">- Semua Kategori -</option>
                                 @foreach($kategori as $item)
-                                    <option value="{{ $item->kategori_id }}">{{ $item->nama_kategori}}</option>
+                                    <option value="{{ $item->kategori_id }}">{{ $item->nama_kategori }}</option>
                                 @endforeach
                             </select>
                             <small class="form-text text-muted">Kategori Barang</small>
@@ -75,34 +34,42 @@
                     </div>
                 </div>
             </div>
-
-            <table class="table table-bordered table-striped table-hover table-sm text-nowrap" id="table_barang">
+            
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_barang">
                 <thead>
                     <tr>
                         <th>ID</th>
                         <th>Kode Barang</th>
                         <th>Nama Barang</th>
-                        <th>Kategori</th>
                         <th>Harga Beli</th>
                         <th>Harga Jual</th>
+                        <th>Kategori</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
             </table>
         </div>
+        <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
     </div>
 @endsection
 
 @push('js')
 <script>
+    function modalAction(url = '') {
+        $('#myModal').load(url, function() {
+            $('#myModal').modal('show');
+        });
+    }
+
+    var dataBarang;
     $(document).ready(function() {
-        var dataBarang = $('#table_barang').DataTable({
+        dataBarang = $('#table_barang').DataTable({
             serverSide: true,
             ajax: {
                 "url": "{{ url('barang/list') }}",
                 "dataType": "json",
                 "type": "POST",
-                "data": function (d) {
+                "data": function(d) {
                     d.kategori_id = $('#kategori_id').val();
                 }
             },
@@ -110,9 +77,9 @@
                 { data: "DT_RowIndex", className: "text-center", orderable: false, searchable: false },
                 { data: "barang_kode", orderable: true, searchable: true },
                 { data: "barang_nama", orderable: true, searchable: true },
+                { data: "harga_beli", orderable: true, searchable: false },
+                { data: "harga_jual", orderable: true, searchable: false },
                 { data: "kategori.nama_kategori", orderable: false, searchable: false },
-                { data: "harga_beli", orderable: true, searchable: false, className: "text-right" },
-                { data: "harga_jual", orderable: true, searchable: false, className: "text-right" },
                 { data: "aksi", orderable: false, searchable: false }
             ]
         });
