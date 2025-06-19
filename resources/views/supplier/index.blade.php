290e454d5c8fc1d5cf5344dc7c2d2372cbfd98dc
nazwanrlwjy <nazwanrlwjy@gmail.com>
2025-06-20 02:25:23 +0700

    Tugas JS 6_m_supplier

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <div class="card card-outline card-primary">
         <div class="card-header">
-            <h3 class="card-title">{{ $page->title }}</h3>
+            <h3 class="card-title">Data Supplier</h3>
             <div class="card-tools">
                 <a class="btn btn-sm btn-primary mt-1" href="{{ url('supplier/create') }}">Tambah</a>
+                <button onclick="modalAction('{{ url('supplier/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
             </div>
         </div>
-
         <div class="card-body">
             @if (session('success'))
                 <div class="alert alert-success">{{ session('success') }}</div>
@@ -18,62 +18,48 @@
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
 
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group row">
-                        <label class="col-1 control-label col-form-label">Filter:</label>
-                        <div class="col-3">
-                            <input type="text" class="form-control" id="supplier_nama" placeholder="Cari Nama Supplier">
-                            <small class="form-text text-muted">Nama Supplier</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
             <table class="table table-bordered table-striped table-hover table-sm" id="table_supplier">
                 <thead>
                     <tr>
                         <th>ID</th>
-                        <th>Kode</th>
-                        <th>Nama</th>
-                        <th>Alamat</th>
+                        <th>Kode supplier</th>
+                        <th>Nama Supplier</th>
                         <th>Telepon</th>
+                        <th>Alamat</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
             </table>
         </div>
+        <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
     </div>
 @endsection
 
-@push('css')
-@endpush
-
 @push('js')
 <script>
+    function modalAction(url = ''){
+        $('#myModal').load(url, function(){
+            $('#myModal').modal('show');
+        });
+    }
+
+    var dataSupplier;
     $(document).ready(function() {
-        var dataSupplier = $('#table_supplier').DataTable({
+        dataSupplier = $('#table_supplier').DataTable({
             serverSide: true,
             ajax: {
                 "url": "{{ url('supplier/list') }}",
                 "dataType": "json",
-                "type": "POST",
-                "data": function(d) {
-                    d.supplier_nama = $('#supplier_nama').val();
-                }
+                "type": "POST"
             },
             columns: [
                 { data: "DT_RowIndex", className: "text-center", orderable: false, searchable: false },
                 { data: "supplier_kode", orderable: true, searchable: true },
                 { data: "supplier_nama", orderable: true, searchable: true },
+                { data: "supplier_telp", orderable: true, searchable: true },
                 { data: "supplier_alamat", orderable: false, searchable: false },
-                { data: "supplier_telp", orderable: false, searchable: false },
                 { data: "aksi", orderable: false, searchable: false }
             ]
-        });
-
-        $('#supplier_nama').on('keyup', function() {
-            dataSupplier.ajax.reload();
         });
     });
 </script>
--- routes/web.php
+++ routes/web.php
@@ -67,9 +67,15 @@ Route::group(['prefix' => 'supplier'], function () {
     Route::post('/list', [SupplierController::class, 'list']);
     Route::get('/create', [SupplierController::class, 'create']);
     Route::post('/', [SupplierController::class, 'store']);
+    Route::get('/create_ajax', [SupplierController::class, 'create_ajax']);
+    Route::post('/ajax', [SupplierController::class, 'store_ajax']);
     Route::get('/{id}', [SupplierController::class, 'show']);
     Route::get('/{id}/edit', [SupplierController::class, 'edit']);
     Route::put('/{id}', [SupplierController::class, 'update']);
+    Route::get('/{id}/edit_ajax', [SupplierController::class, 'edit_ajax']);
+    Route::put('/{id}/update_ajax', [SupplierController::class, 'update_ajax']);
+    Route::get('/{id}/delete_ajax', [SupplierController::class, 'confirm_ajax']);
+    Route::delete('/{id}/delete_ajax', [SupplierController::class, 'delete_ajax']);
     Route::delete('/{id}', [SupplierController::class, 'destroy']);
 });
 
@@ -78,8 +84,14 @@ Route::group(['prefix' => 'barang'], function () {
     Route::post('/list', [BarangController::class, 'list']);
     Route::get('/create', [BarangController::class, 'create']);
     Route::post('/', [BarangController::class, 'store']);
+    Route::get('/create_ajax', [BarangController::class, 'create_ajax']);
+    Route::post('/ajax', [BarangController::class, 'store_ajax']);
     Route::get('/{id}', [BarangController::class, 'show']);
     Route::get('/{id}/edit', [BarangController::class, 'edit']);
     Route::put('/{id}', [BarangController::class, 'update']);
+    Route::get('/{id}/edit_ajax', [BarangController::class, 'edit_ajax']);
+    Route::put('/{id}/update_ajax', [BarangController::class, 'update_ajax']);
+    Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);
+    Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
     Route::delete('/{id}', [BarangController::class, 'destroy']);
 });
